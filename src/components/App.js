bf7698c3c29beea611ec8b359eb7eb0fb6e86352
Anthony Dang <anthonydang1999@gmail.com>
2024-01-18 18:59:08 -0800

    Remove CenteredContainer class from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,25 @@ import Signup from "./authentication/Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
+import Profile from "./authentication/Profile"
 import Login from "./authentication/Login"
 import PrivateRoute from "./authentication/PrivateRoute"
 import ForgotPassword from "./authentication/ForgotPassword"
 import UpdateProfile from "./authentication/UpdateProfile"
-import CenteredContainer from "./authentication/CenteredContainer"
 
 function App() {
   return (
-    <CenteredContainer>
-      <Router>
-        <AuthProvider>
-          <Switch>
-            <PrivateRoute exact path="/" component={Dashboard} />
-            <PrivateRoute path="/update-profile" component={UpdateProfile} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/login" component={Login} />
-            <Route path="/forgot-password" component={ForgotPassword} />
-          </Switch>
-        </AuthProvider>
-      </Router>
-    </CenteredContainer>
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <PrivateRoute exact path="/" component={Profile} />
+          <PrivateRoute path="/update-profile" component={UpdateProfile} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/forgot-password" component={ForgotPassword} />
+        </Switch>
+      </AuthProvider>
+    </Router>
   )
 }
 
