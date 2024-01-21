62d4b392d34854fc79d26c7b72f868486c92b41a
Anthony Dang <anthonydang1999@gmail.com>
2024-01-20 18:05:00 -0800

    Creating and modifying .js files to make the web-app home page

---
+++ src/components/google-drive/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { Container } from 'react-bootstrap'
+
+export default function Dashboard() {
+  return <>
+    <Navbar/>
+    <Container fluid>
+        
+    </Container>
+  </>
+}
