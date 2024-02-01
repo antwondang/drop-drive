48e384de0f50503c5e2a0adae72575e0611e17a0
Anthony Dang <anthonydang1999@gmail.com>
2024-01-31 18:28:05 -0800

    Adding folder button onto the dashboard in .js files

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { Container } from 'react-bootstrap'
+import AddFolderButton from './AddFolderButton'
 
 export default function Dashboard() {
   return <>
     <Navbar/>
     <Container fluid>
-        
+        <AddFolderButton />
     </Container>
   </>
 }
