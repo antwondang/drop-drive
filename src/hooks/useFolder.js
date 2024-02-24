7ce270a0fc62c024984572de9c9b5b23783b75e2
Anthony Dang <anthonydang1999@gmail.com>
2024-02-23 19:48:01 -0800

    Create reducer and default state for folder use in useFolder.js

--- src/hooks/useFolder.js
+++ src/hooks/useFolder.js
@@ -1,3 +1,14 @@
-export function uesFolder(folderId = null){
-    
+import { useReducer } from "react"
+
+function reducer() {
+
+}
+
+export function uesFolder(folderId = null, folder = null){
+    const [state, dispatch] = useReducer(reducer, {
+        folderId, 
+        folder,
+        childFolders: [],
+        childFiles: []
+    })
 }
