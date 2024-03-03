cd1e3616c99d4fd13fa178d8971e9a9e8aa7544f
Anthony Dang <anthonydang1999@gmail.com>
2024-03-02 18:27:37 -0800

    Simplify childFolder name in dashboard.js

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -6,20 +6,20 @@ import { useFolder } from '../../hooks/useFolder'
 import Folder from './Folder'
 
 export default function Dashboard() {
-    const { folder, childFolders } = useFolder()
+    const { folder, childFolder } = useFolder()
 
   return (
     <>
     <Navbar/>
     <Container fluid>
         <AddFolderButton currentFolder={folder}/>
-        {childFolders.length > 0 && (
+        {childFolder.length > 0 && (
             <div className="d-flex flex-wrap">
-                {childFolders.map(folder => {
+                {childFolder.map(folder => {
                     <div key={folder.id} style={{ maxWidth: '250px' }}
                     className="p-2">
-                    
-                    <Folder folder={childFolders}/>
+
+                    <Folder folder={childFolder}/>
                     </div>
                 })}
             </div>
