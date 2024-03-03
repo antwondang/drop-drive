38004694e3a8025f3db91f44e6afe86a3152a2ed
Anthony Dang <anthonydang1999@gmail.com>
2024-03-02 18:41:02 -0800

    Undo changes for js files.

--- src/components/google-drive/Folder.js
+++ src/components/google-drive/Folder.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
+import { Button } from 'react-bootstrap'
 
 export default function Folder({ folder }) {
-  return (
-    <div>
-        {folder.name}
-    </div>
-  )
+  return <Button as={Link}>
+  <FontAwesomeIcon icon={faFolder} className='mr-2'/> {folder.name}</Button>
 }
