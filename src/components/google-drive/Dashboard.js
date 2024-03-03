4313a43a74e287aabbfa45f7e7c91696eb659b8d
Anthony Dang <anthonydang1999@gmail.com>
2024-03-02 18:21:10 -0800

    Create access for child folders in different js files.

--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -3,15 +3,27 @@ import Navbar from './Navbar'
 import { Container } from 'react-bootstrap'
 import AddFolderButton from './AddFolderButton'
 import { useFolder } from '../../hooks/useFolder'
+import Folder from './Folder'
 
 export default function Dashboard() {
-    const { folder } = useFolder()
+    const { folder, childFolders } = useFolder()
 
   return (
     <>
     <Navbar/>
     <Container fluid>
         <AddFolderButton currentFolder={folder}/>
+        {childFolders.length > 0 && (
+            <div className="d-flex flex-wrap">
+                {childFolders.map(folder => {
+                    <div key={folder.id} style={{ maxWidth: '250px' }}
+                    className="p-2">
+                    
+                    <Folder folder={childFolders}/>
+                    </div>
+                })}
+            </div>
+        )}
     </Container>
   </>
   )
